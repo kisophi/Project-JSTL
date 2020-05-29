fa21518ec93984323bce473bda8f34cf13344526
Akira Matsumoto <akiramatsumotoo@gmail.com>
2020-05-28 21:24:07 -0300

    added user

---
+++ src/main/java/br/com/akira/model/User.java
@@ -0,0 +1,42 @@
+package br.com.akira.model;
+
+public class User {
+
+	private int userid;
+	private String name;
+	private String login;
+	private String pass;
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+}
