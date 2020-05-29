695408dec0c7c005366b22f695ad0f8be9ca4fc3
Akira Matsumoto <akiramatsumotoo@gmail.com>
2020-05-29 10:06:06 -0300

    Added script SQL

---
+++ scriptSQL.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE	dbjava;
+
+USE dbjava;
+
+CREATE TABLE user(
+	userid int AUTO_INCREMENT,
+    name VARCHAR(255),
+    login VARCHAR(255) UNIQUE NOT NULL,
+    pass VARCHAR(255),
+    PRIMARY KEY(userid)
+);
+
+INSERT INTO user VALUES(null,'Akira','aki','111');
+INSERT INTO user VALUES(null,'Leandro','leo','222');
+INSERT INTO user VALUES(null,'João','joa','333');
+INSERT INTO user VALUES(null,'Pedro','ped','444');
+INSERT INTO user VALUES(null,'Maria','mar','555');
+INSERT INTO user VALUES(null,'Carlos','car','666');
+INSERT INTO user VALUES(null,'Fabio','fab','777');
+
+SELECT*FROM user;
