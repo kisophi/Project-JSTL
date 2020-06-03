0031c0e3d656f50bc899884e101a97cd108b9aa1
Akira Matsumoto <akiramatsumotoo@gmail.com>
2020-06-03 13:05:14 -0300

    Implement method deleteUser

--- src/main/java/br/com/akira/model/UserDAO.java
+++ src/main/java/br/com/akira/model/UserDAO.java
@@ -1,11 +1,62 @@
 package br.com.akira.model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import br.com.akira.util.ConnectionFactory;
 
 public class UserDAO {
 
-	Connection Conn = ConnectionFactory.getConnecitonMySql();
+	Connection conn = ConnectionFactory.getConnecitonMySql();
+	PreparedStatement ps = null;
+
+	/**
+	 * Add User
+	 * 
+	 * @param User u
+	 */
+	public void addUser(User u) {
+		try {
+			ps = conn.prepareStatement("INSERT INTO user VALUES(null,?,?,?)");
+			ps.setString(1, u.getName());
+			ps.setString(2, u.getLogin());
+			ps.setString(3, u.getPass());
+			ps.execute();
+			System.out.println("Added User"+toString());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
+	/**
+	 * Delete User
+	 * @param Integer
+	 */
+	public void deleteUser(Integer id) {
+		try {
+			ps = conn.prepareStatement("DELETE user FROM WHERE userid=?");
+			ps.setInt(1, id);
+			ps.execute();
+			System.out.println("Delete User"+toString());
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
